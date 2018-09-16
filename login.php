6cee8296b49fa652a350873832c700efc6b654fd
Kleber2018 <42260292+Kleber2018@users.noreply.github.com>
2018-09-16 20:20:15 -0300

    Lista 4

--- login.php
+++ login.php
@@ -1,2 +1,33 @@
-# Lista4-PHP
-Lista 4 WebServidor
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Lista 4 Login</title>
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Custom styles for this template -->
+        <link href="css/signin.css" rel="stylesheet">
+    </head>
+
+    <body class="text-center">
+        <form class="form-signin" method="POST">
+            <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+            <label for="inputEmail" class="sr-only">Usuário</label>
+            <input type="text" id="inputusuario" class="form-control" placeholder="Usuário" required autofocus>
+            <label for="inputPassword" class="sr-only">Senha</label>
+            <input type="password" id="inputsenha" class="form-control" placeholder="Senha" required>
+            <div class="checkbox mb-3">
+                <label>
+                <input type="checkbox" value="remember-me"> Relembrar
+                </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2018</p>
+        </form>
+    </body>
+</html>
